64a804ee12f17864d51a9a3d787590d3f55d16e3
brysbroughton <brysbroughton24@gmail.com>
2022-03-22 14:23:46 -0400

    removed old site files

--- global.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	(document.addEventListener || document.attachEvent)
-	.call(document, 'DOMContentLoaded', function () {
-		Array.prototype.forEach
-		.call(document.getElementsByClassName('sidenav'), function (e) {
-			M.Sidenav.init(e);
-		});
-	});
-})()
